3fcd3b25baaee403f529e3a585a9a3c39e26384c
Francis Dominic Fajardo <fajardo@duck.com>
2026-09-26 02:04:20 +0800

    Remove inset above search bar

--- app/src/main/java/io/github/dovecoteescapee/byedpi/ui/screens/VpnAppsFilterScreen.kt
+++ app/src/main/java/io/github/dovecoteescapee/byedpi/ui/screens/VpnAppsFilterScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -113,6 +114,7 @@ fun VpnAppsFilterScreen(
             },
             expanded = false,
             onExpandedChange = {},
+            windowInsets = WindowInsets(0.dp),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp, vertical = 8.dp),
