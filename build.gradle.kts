fcba0271c19e3dac502ead01c57326f795ec33d1
Francis Dominic Fajardo <fajardo@duck.com>
2026-09-26 14:36:24 +0800

    Update com.android.application to 9.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "9.3.1" apply false
+    id("com.android.application") version "9.4.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
     id("com.diffplug.spotless") version "7.0.2" apply false
 }
